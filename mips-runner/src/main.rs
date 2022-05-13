124b904d20ace57bfd10662f8560a952d1a26fd6
caojiafeng <funfriendcjf@gmail.com>
2022-05-13 16:32:28 +0800

    update

--- mips-runner/src/main.rs
+++ mips-runner/src/main.rs
@@ -2,7 +2,5 @@ use unicorn_engine::Unicorn;
 use unicorn_engine::unicorn_const::{Arch, Mode};
 
 fn main() {
-    println!("Hello, world!");
     let engine = Unicorn::new(Arch::MIPS,Mode::MIPS32).unwrap();
-    engine.mem_map()
 }
